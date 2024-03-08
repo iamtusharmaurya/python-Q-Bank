e9795d81394e41f0621af81c821af87ace38e3ff
Tushar Maurya <100959310+devtusharmaurya@users.noreply.github.com>
2024-03-08 17:11:29 +0530

    change

--- 004_python.py
+++ 004_python.py
@@ -1,12 +1,11 @@
-n=121
+n=int(input("Enter number:"))
 temp=n
 rev=0
 while(n>0):
-    dig=temp%10
-    rev=rev*+dig
-    temp=temp//10
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
 if(temp==rev):
-    print("number is pailaindrom")
+    print("The number is a palindrome!")
 else:
-    print("number is not pailaindrom")    
-    
+    print("The number isn't a palindrome!")
